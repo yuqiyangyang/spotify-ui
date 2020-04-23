6b77833ce63c94f3845f0969030a270c055a9c64
yuqi <yuqi.yang@mail.mcgill.ca>
2020-04-23 15:32:40 -0400

    main page styling

---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="upperNav"> dummy text</div>
+      <div className="mainContent">
+        <h1>Uniquely yours</h1>
+        <div className="cardWrap">
+          <div className="card">
+            <div className="cardImage">
+              <img
+                src="https://images.unsplash.com/photo-1495231916356-a86217efff12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
+                alt="Pic 1"
+              />
+            </div>
+            <div className="cardContent">
+              <h3>Liked Songs</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
