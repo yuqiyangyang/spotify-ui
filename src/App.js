a52e16c74bab4f07363bdc8307b34d2b7584bc50
yuqi <yuqi.yang@mail.mcgill.ca>
2020-04-22 22:22:28 -0400

    page layout setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.scss";
+import Nav from "./components/Nav";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="outerWrap">
+      <div className="App">
+        <Nav />
+      </div>
+      <div className="musicControls">Music controls </div>
+    </div>
+  );
 }
 
 export default App;
